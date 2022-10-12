542861999a6a04d6df40fbb0426fc0e635f6f17b
Santifer1 <78063333+Santifer1@users.noreply.github.com>
2022-10-12 10:38:48 -0700

    Lab in process

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import Animal.Animal;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Animal myPet = new Animal();
+
     }
 }
